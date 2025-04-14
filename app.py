d08ab459623d57aa13ac35aa31650879e4b07729
angelajonyl <angelajonyl.reyes@dcmail.ca>
2025-04-14 17:38:57 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
+import os
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_cloud():
-  return 'Hello from Reyes ECS Container!!'
+def hello():
+    return "Hello from Reyes ECS Container!"
 
-app.run(host='0.0.0.0')
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 8080))  # default to 8080
+    app.run(host='0.0.0.0', port=port)
